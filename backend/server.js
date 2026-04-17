7e2c8b331edd32d73cb802a612fe45d52c4bec35
Sayantan Bera <sayantanbera25@gmail.com>
2026-04-17 13:44:23 +0530

    fix: update server port configuration and correct import case in main.jsx

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,6 @@ const connectDB = require("./src/config/database");
 
 connectDB();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port " + process.env.PORT);
 });
